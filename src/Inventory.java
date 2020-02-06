fbaf00cc825b47cc32532ae20b54076fe6caa891
Lukas <doom7@web.de>
2020-02-06 23:57:28 +0100

    added a link to continue tomorrow

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,4 +1,6 @@
 public class Inventory {
     // https://scottlilly.com/how-to-build-stackable-inventory-for-a-game-in-c/
     // https://scottlilly.com/build-a-cwpf-rpg/lesson-06-1-creating-the-quest-factory/
+
+    //https://www.gamedev.net/forums/topic/654197-small-inventory-system-java-novice/
 }
