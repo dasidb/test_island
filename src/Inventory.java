972be1c2922d7e851a9702840b5df83047ba4fe7
Lukas <doom7@web.de>
2020-02-09 23:30:12 +0100

    added a link to continue tomorrow

--- src/Inventory.java
+++ src/Inventory.java
@@ -2,6 +2,7 @@
 // https://scottlilly.com/build-a-cwpf-rpg/lesson-06-1-creating-the-quest-factory/
 
 //https://www.gamedev.net/forums/topic/654197-small-inventory-system-java-novice/
+//https://www.gamedev.net/articles/programming/general-and-gameplay-programming/item-management-systems-r2163/
 public class Inventory {
     private int inventorySlots = 10;
 
