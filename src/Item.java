84d1b11ce66a5647fc39e8c9d5fdef459b3f0624
Lukas.Kapust <dasidb>
2020-02-10 16:44:41 +0100

    .

--- src/Item.java
+++ src/Item.java
@@ -3,11 +3,14 @@ public abstract class Item {
 
     public int itemID;
     public String name;
+    public ItemType type;
+    public String description;
     public int maximumStackSize;
 
-    protected Item(int itemID, String name, int maximumStackSize){
+    protected Item(int itemID, String name, ItemType type, int maximumStackSize){
         this.itemID = itemID;
         this.name = name;
+        this.type = type;
         this.maximumStackSize = maximumStackSize;
     }
 
---
+++ src/ItemType.java
@@ -0,0 +1,11 @@
+public enum ItemType {
+    Weapon(0),
+    Tool(10),
+    Consumable(11),
+    CraftingMaterial(12),
+    Junk(100);
+
+    private ItemType(int value){
+
+    }
+}
